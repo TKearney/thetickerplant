1d91f9c777bc8a3b42cff1a27f2ca5511dfaa836
EC2 Default User <ec2-user@ip-172-31-5-210.ap-southeast-2.compute.internal>
2017-11-05 10:46:30 +0000

    SEO

--- server/kdbFiles/run.sh
+++ server/kdbFiles/run.sh
@@ -4,7 +4,7 @@ cd kdb-tick-master/
 echo starting tp 
 eval ${q} tick.q hdb ../ -p ${tpPort} -U ../../${kdbUserPassFile} &
 echo starting rdb
-eval ${q} tick/r.q localhost:${tpPort}:${user}:${pass} -p ${rdbPort} -U ../../${kdbUserPassFile} &
+eval ${q} tick/r.q localhost:${tpPort}:${user}:${pass} :${hdbPort} -p ${rdbPort} -U ../../${kdbUserPassFile} &
 echo starting hdb
 eval ${q} hdb.q -p ${hdbPort} &
 echo starting gw
