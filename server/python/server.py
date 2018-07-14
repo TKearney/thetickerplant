f110dade7d4001d7760797d0568bd928ae6387bb
EC2 Default User <ec2-user@ip-172-31-5-210.ap-southeast-2.compute.internal>
2018-07-14 02:56:41 +0000

    Websocket iot working

--- server/python/server.py
+++ server/python/server.py
@@ -31,15 +31,21 @@ def connectUpstream():
 
 connectUpstream()
 
-def recvSensor():
+#handle websocket connections
+connections = set()
+n=0
+
+async def recvSensor():
     data = conn.recv(BUFFER_SIZE)
+    #print(data)
     if not data:
         print('not data')
         conn.shutdown(1)
         conn.close()
         connectUpstream()
-    return data
+    return data.decode()
 
+data = recvSensor()
 #conn, addr = s.accept()
 #print('Connection address:', addr)
 # WS server that sends messages at random intervals
@@ -47,25 +53,79 @@ import asyncio
 import datetime
 import random
 import websockets
-async def sensor(websocket, path):
+async def handler(websocket, path):
+    #global n
+    #n=n+1
+    connections.add(websocket)
+    #print("adding subscriber #", n)
+    #try:
+        #async for msg in websocket:
+            #pass  # ignore
+    #except websockets.ConnectionClosed:
+        #pass
+    #finally:
+        #print("removing subscriber #", n)
+        #connections.remove(websocket)
+    print(connections)
+    #data = recvSensor()
+    #await websocket.send(data.decode())
+    await asyncio.sleep(0.001)
     while True:
+        await asyncio.sleep(0.01)
         #now = datetime.datetime.utcnow().isoformat() + 'Z'
         #await websocket.send(now)
         #await asyncio.sleep(random.random() * 3)
         #data = conn.recv(BUFFER_SIZE)
         #print(data)
-        data = recvSensor()
+        data = await recvSensor()
+       #  = data.decode()
         print(data)
-        await websocket.send(data.decode())
+        #await websocket.send(data.decode())
+        for websocket in connections:
+            if not websocket.open:
+                connections.remove(websocket)
+                break                
+            await websocket.send(data)
 
-start_server = websockets.serve(sensor, '0.0.0.0', 5009)
-
+async def wsSend():
+    print('wssend')
+    asyncio.get_event_loop().run_forever()
+    while True:
+        print('recv data')
+        await recvSensor()
+        print(connections)
+        for websocket in connections:
+            websocket.send(recvSensor())
+print('starting server')
+start_server = websockets.serve(handler, '0.0.0.0', 5009)
+print('A')
 asyncio.get_event_loop().run_until_complete(start_server)
+print('B')
 asyncio.get_event_loop().run_forever()
+    
+print('C')
+
+#loop = asyncio.get_event_loop()
+#loop.run_until_complete(wsSend())
+#print('D')
+#loop.run_forever()
+#print('E')
+#try:
+    #asyncio.ensure_future(wsSend())
+    #data = asyncio.async(recvSensor())
+    #wsSend()
+    #loop.run_forever()
+#except KeyboardInterrupt:
+    #pass
+#finally:
+    #print('closing loop')
+    #loop.close()
+#asyncio.get_event_loop().run_forever()
 
 
 #while 1:
-#    data = conn.recv(BUFFER_SIZE)
+    #data = recvSensor()
+
 #    if not data: break
 #    print(data.decode())
     #conn.send(data)  # echo
